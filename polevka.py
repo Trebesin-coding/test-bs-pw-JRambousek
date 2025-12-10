27c8fcca64e6752c671a19b75be10de05fba7771
JRambousek <rambousek122@trebesin.cz>
2025-12-10 09:01:43 +0100

    polevka
    Updated the main function to fetch and parse HTML content from a specified URL using BeautifulSoup. Extracted and printed the text of the first four <h2> elements from the parsed HTML.

--- polevka.py
+++ polevka.py
@@ -1,14 +1,24 @@
 from bs4 import BeautifulSoup
+import json
+# from recept.json import recept
 import requests
 
 
 def main():
+    url = "https://souhrada.github.io/bsoup-exam/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser") #html.parser = jednoduché označení pro response.text
 
+    pokus_1 = soup.select("h2")[0]
+    pokus_2 = soup.select("h2")[1]
+    pokus_3 = soup.select("h2")[2]
+    pokus_4 = soup.select("h2")[3]
+    finalni_produkt = pokus_1.text, pokus_2.text, pokus_3.text, pokus_4.text
 
-    # response = requests.get(url)
+    print(pokus_1.text, pokus_2.text, pokus_3.text, pokus_4.text)
 
-    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
-
+    # json.dump(finalni_produkt, recept, indent=4)
+    # json.load(recept)
 
 if __name__ == "__main__":
     main()
