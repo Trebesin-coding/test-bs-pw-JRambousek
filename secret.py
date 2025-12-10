0313bc0125a405149a0eb7a1af79b50a4663eea6
JRambousek <rambousek122@trebesin.cz>
2025-12-10 09:02:46 +0100

    secret
    Added login functionality using Playwright to automate login process.

--- secret.py
+++ secret.py
@@ -1,21 +1,23 @@
 from playwright.sync_api import sync_playwright
 import os
 
-
-
+login = "Jarmil"
+password = "Admin123"
 
 def main():
 
-    # with sync_playwright() as p:
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://souhrada.github.io/playwright-exam/")
+    
+        page.fill("input#login", login)
+        page.fill("input#pass", password)
+        page.click(".login-btn")
+        page.locator(".psst hidden").text_content()
 
-        # browser = p.chromium.launch(headless=False)
-
-        # !!!
-        # na page.locator(selector) se dá použít funkce .text_content(), která vypíše text daného prvku
-        # !!!
-
-
-        # browser.close()
+        page.wait_for_timeout(20000)
+        browser.close()
     
 
 if __name__ == "__main__":
